cfa092b342a08c3fc89dee0536c4c658cdf91137
RyanShim2000 <ryanwonshim@gmail.com>
2023-11-25 12:56:37 -0500

    first commit

---
+++ reddit-tool/src/main/java/com/reddittool/RedditToolApplication.java
@@ -0,0 +1,13 @@
+package com.reddittool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditToolApplication.class, args);
+	}
+
+}
---
+++ reddit-tool/src/test/java/com/reddittool/RedditToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddittool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
